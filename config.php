c19d4803eeb3600bbd06d22a9f10f31f8f033f84
David Foale <mail@davidfoale.info>
2016-07-27 19:47:36 +0100

    Add files via upload
    revised index.php etc

--- config.php
+++ config.php
@@ -5,7 +5,13 @@ define('DB_HOST', 'localhost'); /*Database Server*/
 define('DB_NAME', 'WEBADMIN'); /*Database Name*/
 define('DB_USER', 'root'); /*Database Username*/
 define('DB_PWD',  ''); /*Database Password*/
+
+
+$siteTitle=  'PlaceHolder_siteTitle';
+$siteFooter=  'PlaceHolder_siteFooter';
 $siteBrand=  'PlaceHolder_siteBrand';
+$siteAddress=  'PlaceHolder_siteAddress';
+$siteShortTitle=  'PlaceHolder_siteShortTitle';
 
   /*Prepare Query to pull current settings from SiteConfig*/
   $sql="SELECT ConfigName
@@ -18,8 +24,13 @@ $siteBrand=  'PlaceHolder_siteBrand';
 /*Run the query, if ConfigName matches ‘siteBrand, then set $siteBrand to ShortTextValue returned in from the query*/
   if ($result = mysqli_query($link,$sql)){
       while ($row = mysqli_fetch_array($result))  {
-        if ($row[0] == 'siteBrand') { $siteBrand = $row[1];}
-     } 
+        if ($row[0] == 'siteTitle') { $siteTitle = $row[1];}
+         if ($row[0] == 'siteFooter') { $siteFooter = $row[1];}
+          if ($row[0] == 'siteBrand') { $siteBrand = $row[1];}
+           if ($row[0] == 'siteAddress') { $siteBrand = $row[1];}
+            if ($row[0] == 'siteShortTitle') { $siteShortTitle = $row[1];}
+         } 
+    
   }
   /*Close database connection*/
 	mysqli_close ( $link );
